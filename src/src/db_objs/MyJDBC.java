93b0dae6e393558888e0c1486d1248a6fc17944d
Tarek Sherif <tareksherif2020@gmail.com>
2024-06-21 15:05:07 +0300

    PROJECT DONE (not updated for a while cause of traveling)

--- src/src/db_objs/MyJDBC.java
+++ src/src/db_objs/MyJDBC.java
@@ -2,101 +2,168 @@ package db_objs;
 
 import java.math.BigDecimal;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class MyJDBC {
     private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/bankapp";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "password";
 
-    public static User validateLogin(String username, String password){
-        try{
+    public static User validateLogin(String username, String password) {
+        try {
             Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
 
-            //create query
+            // Create query
             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
             preparedStatement.setString(1, username);
             preparedStatement.setString(2, password);
 
             ResultSet resultSet = preparedStatement.executeQuery();
 
-            if(resultSet.next()){
-                int userId = resultSet.getInt(("id"));
+            if (resultSet.next()) {
+                int userId = resultSet.getInt("id");
                 BigDecimal currentBalance = resultSet.getBigDecimal("current_balance");
 
                 return new User(userId, username, password, currentBalance);
-
-
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
         return null;
     }
 
-    //register, true = yes, false = no
-    public static boolean register(String username, String password){
-
-        try{
-
-            if(!checkUser(username)){
+    public static boolean register(String username, String password) {
+        try {
+            if (!checkUser(username)) {
                 Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
 
                 PreparedStatement preparedStatement = connection.prepareStatement(
-                        "INSERT INTO users(username, password) " +
-                                "VALUES(?, ?)"
+                        "INSERT INTO users(username, password, current_balance) VALUES(?, ?, ?)"
                 );
 
                 preparedStatement.setString(1, username);
                 preparedStatement.setString(2, password);
+                preparedStatement.setBigDecimal(3, BigDecimal.ZERO);
 
                 preparedStatement.executeUpdate();
                 return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
+    private static boolean checkUser(String username) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE username = ?");
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            preparedStatement.setString(1, username);
+            return resultSet.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean addTransactionToDatabase(Transaction transaction) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+             PreparedStatement insertTransaction = connection.prepareStatement(
+                     "INSERT INTO transactions(user_id, transaction_type, transaction_amount, transaction_date) VALUES(?, ?, ?, NOW())")) {
+
+            insertTransaction.setInt(1, transaction.getUserId());
+            insertTransaction.setString(2, transaction.getTransactionType());
+            insertTransaction.setBigDecimal(3, transaction.getTransactionAmount());
+
+            insertTransaction.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean updateCurrentBalance(User user) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+             PreparedStatement updateBalance = connection.prepareStatement("UPDATE users SET current_balance = ? WHERE id = ?")) {
+
+            updateBalance.setBigDecimal(1, user.getCurrentBalance());
+            updateBalance.setInt(2, user.getId());
+
+            updateBalance.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean transfer(User user, String transferredUsername, BigDecimal transferAmount) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+
+            PreparedStatement queryUser = connection.prepareStatement("SELECT * FROM users WHERE username = ?");
+            queryUser.setString(1, transferredUsername);
+            ResultSet resultSet = queryUser.executeQuery();
+
+            if (resultSet.next()) {
+                User transferredUser = new User(resultSet.getInt("id"), transferredUsername,
+                        resultSet.getString("password"), resultSet.getBigDecimal("current_balance"));
+
+                // Create transaction
+                Transaction transferTransaction = new Transaction(user.getId(), "Transfer", transferAmount, null);
+                Transaction receivedTransaction = new Transaction(transferredUser.getId(), "Transfer", transferAmount, null);
+
+                // Update transferred user balance
+                transferredUser.setCurrentBalance(transferredUser.getCurrentBalance().add(transferAmount));
+                updateCurrentBalance(transferredUser);
+
+                // Update user current balance
+                user.setCurrentBalance(user.getCurrentBalance().subtract(transferAmount));
+                updateCurrentBalance(user);
+
+                // Add transactions to the database
+                addTransactionToDatabase(transferTransaction);
+                addTransactionToDatabase(receivedTransaction);
+
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    // get all past transactions
+    public static ArrayList <Transaction> getPastTransaction(User user){
+        ArrayList<Transaction> pastTransactions = new ArrayList<>();
+        try{
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+
+            PreparedStatement selectAllTransaction = connection.prepareStatement(
+                    "SELECT * FROM transactions WHERE user_id = ?"
+            );
+            selectAllTransaction.setInt(1, user.getId());
+
+            ResultSet resultSet = selectAllTransaction.executeQuery();
+
+            //iterate through the results if any
+            while(resultSet.next()){
+                Transaction transaction = new Transaction(
+                        user.getId(),
+                        resultSet.getString("transaction_type"),
+                        resultSet.getBigDecimal("transaction_amount"),
+                        resultSet.getDate("transaction_date")
+                );
+
+                //store it into an arraylist
+                pastTransactions.add(transaction);
             }
 
         }catch(SQLException e){
             e.printStackTrace();
         }
 
-        return false;
-
+        return pastTransactions;
     }
-
-
-    // true = already exists, false = doesn't exist
-    private static boolean checkUser(String username) {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-
-        try {
-            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-            preparedStatement = connection.prepareStatement(
-                    "SELECT * FROM users WHERE username = ?"
-            );
-            preparedStatement.setString(1, username);
-            resultSet = preparedStatement.executeQuery();
-
-            if (resultSet.next()) {
-                return true; // User exists
-            } else {
-                return false; // User does not exist
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false; // In case of an error, you may decide the default behavior
-        } finally {
-            try {
-                if (resultSet != null) resultSet.close();
-                if (preparedStatement != null) preparedStatement.close();
-                if (connection != null) connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-
 }
--- src/src/guis/BankingAppDialog.java
+++ src/src/guis/BankingAppDialog.java
@@ -1,15 +1,27 @@
 package guis;
 
+import db_objs.MyJDBC;
+import db_objs.Transaction;
 import db_objs.User;
-import javax.swing.*;
 
-public class BankingAppDialog extends JDialog{
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class BankingAppDialog extends JDialog implements ActionListener {
     private User user;
     private BankingAppGui bankingAppGui;
+    private JLabel balanceLabel, enterAmountLabel, enterUserLabel;
+    private JTextField enterAmountField, enterUserField;
+    private JButton actionButton;
+    private JPanel pastTransactionPanel;
+    private ArrayList<Transaction> pastTransactions;
 
-    public BankingAppDialog(BankingAppGui bankingAppGui, User user){
-
-        setSize(400,400);
+    public BankingAppDialog(BankingAppGui bankingAppGui, User user) {
+        setSize(400, 400);
         setModal(true);
         setLocationRelativeTo(bankingAppGui);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -18,9 +30,190 @@ public class BankingAppDialog extends JDialog{
 
         this.bankingAppGui = bankingAppGui;
         this.user = user;
-
-
     }
 
+    public void addCurrentBalanceAndAmount() {
+        balanceLabel = new JLabel("Balance: $" + user.getCurrentBalance());
+        balanceLabel.setBounds(0, 10, getWidth() - 20, 20);
+        balanceLabel.setFont(new Font("Dialog", Font.BOLD, 16));
+        balanceLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(balanceLabel);
 
+        enterAmountLabel = new JLabel("Enter Amount:");
+        enterAmountLabel.setBounds(0, 50, getWidth() - 20, 20);
+        enterAmountLabel.setFont(new Font("Dialog", Font.BOLD, 16));
+        enterAmountLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(enterAmountLabel);
+
+        enterAmountField = new JTextField();
+        enterAmountField.setBounds(15, 80, getWidth() - 50, 40);
+        enterAmountField.setFont(new Font("Dialog", Font.BOLD, 20));
+        enterAmountField.setHorizontalAlignment(SwingConstants.CENTER);
+        add(enterAmountField);
+    }
+
+    public void addActionButton(String actionButtonType) {
+        actionButton = new JButton(actionButtonType);
+        actionButton.setBounds(15, 300, getWidth() - 50, 40);
+        actionButton.setFont(new Font("Dialog", Font.BOLD, 20));
+        actionButton.addActionListener(this);
+        add(actionButton);
+    }
+
+    public void addUserField() {
+        enterUserLabel = new JLabel("Enter User:");
+        enterUserLabel.setBounds(0, 160, getWidth() - 20, 20);
+        enterUserLabel.setFont(new Font("Dialog", Font.BOLD, 16));
+        enterUserLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(enterUserLabel);
+
+        enterUserField = new JTextField();
+        enterUserField.setBounds(15, 190, getWidth() - 50, 40);
+        enterUserField.setFont(new Font("Dialog", Font.BOLD, 20));
+        enterUserField.setHorizontalAlignment(SwingConstants.CENTER);
+        add(enterUserField);
+    }
+
+    public void addPastTransactionComponents(){
+        //container to store each transaction
+        pastTransactionPanel = new JPanel();
+
+        //1x1 layout
+        pastTransactionPanel.setLayout(new BoxLayout(pastTransactionPanel, BoxLayout.Y_AXIS));
+
+        //make container scrollable
+        JScrollPane scrollPane = new JScrollPane(pastTransactionPanel);
+
+        //displays the vertical scroll only when needed
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBounds(0, 20, getWidth() - 15, getHeight() - 15);
+
+        //call jdbc method to get all the transactions from a user
+        pastTransactions = MyJDBC.getPastTransaction(user);
+
+        //iterate through the list and add it to the gui
+        for(int i = 0; i < pastTransactions.size(); i++){
+            //store current transaction
+            Transaction pastTransaction = pastTransactions.get(i);
+
+            //create a container to store a singular component
+            JPanel pastTransactionContainer = new JPanel();
+            pastTransactionContainer.setLayout(new BorderLayout());
+
+            //transaction type label
+            JLabel pastTransactionTypeLabel = new JLabel(pastTransaction.getTransactionType());
+            pastTransactionTypeLabel.setFont(new Font("Dialog", Font.BOLD, 20));
+
+            //transaction amount label
+            JLabel pastTransactionAmountLabel = new JLabel(String.valueOf(pastTransaction.getTransactionAmount()));
+            pastTransactionAmountLabel.setFont(new Font("Dialog", Font.BOLD, 20));
+
+            //transaction date label
+            JLabel pastTransactionDateLabel = new JLabel(String.valueOf(pastTransaction.getTransactionDate()));
+            pastTransactionDateLabel.setFont(new Font("Dialog", Font.BOLD, 20));
+
+            //add them to the container
+            pastTransactionContainer.add(pastTransactionTypeLabel, BorderLayout.WEST);
+            pastTransactionContainer.add(pastTransactionAmountLabel, BorderLayout.EAST);
+            pastTransactionContainer.add(pastTransactionDateLabel, BorderLayout.SOUTH);
+
+            //white background color to each container
+            pastTransactionContainer.setBackground(Color.white);
+
+            //add borders
+            pastTransactionContainer.setBorder(BorderFactory.createLineBorder(Color.black));
+
+            //add transaction component to the transaction panel
+            pastTransactionPanel.add(pastTransactionContainer);
+        }
+
+        //add to the dialog
+        add(scrollPane);
+    }
+
+    private void handleTransaction(String transactionType, BigDecimal amountVal) {
+        if (amountVal.compareTo(BigDecimal.ZERO) <= 0) {
+            JOptionPane.showMessageDialog(this, "Error: Amount must be greater than zero.");
+            return;
+        }
+
+        Transaction transaction;
+        if (transactionType.equalsIgnoreCase("Deposit")) {
+            user.setCurrentBalance(user.getCurrentBalance().add(amountVal));
+            transaction = new Transaction(user.getId(), transactionType, amountVal, null);
+        } else {
+            user.setCurrentBalance(user.getCurrentBalance().subtract(amountVal));
+            transaction = new Transaction(user.getId(), transactionType, amountVal.negate(), null);
+        }
+
+        if (MyJDBC.addTransactionToDatabase(transaction) && MyJDBC.updateCurrentBalance(user)) {
+            JOptionPane.showMessageDialog(this, transactionType + " Successful!");
+            resetFieldsAndUpdateCurrentBalance();
+        } else {
+            JOptionPane.showMessageDialog(this, transactionType + " Failed...");
+        }
+    }
+
+    private void handleTransfer(User user, String transferredUser, BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            JOptionPane.showMessageDialog(this, "Error: Amount must be greater than zero.");
+            return;
+        }
+
+        int result = user.getCurrentBalance().compareTo(amount);
+        if (result < 0) {
+            JOptionPane.showMessageDialog(this, "Error: Insufficient funds.");
+            return;
+        }
+
+        if (MyJDBC.transfer(user, transferredUser, amount)) {
+            JOptionPane.showMessageDialog(this, "Transfer Successful!");
+            resetFieldsAndUpdateCurrentBalance();
+        } else {
+            JOptionPane.showMessageDialog(this, "Transfer Failed...");
+        }
+    }
+
+    private void resetFieldsAndUpdateCurrentBalance() {
+        enterAmountField.setText("");
+        if (enterUserField != null) {
+            enterUserField.setText("");
+        }
+
+        // Update the current balance on the dialog page and the main GUI
+        balanceLabel.setText("Balance: $" + user.getCurrentBalance());
+        bankingAppGui.getCurrentBalanceField().setText("$" + user.getCurrentBalance());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String buttonPressed = e.getActionCommand();
+        BigDecimal amountVal;
+
+        try {
+            amountVal = new BigDecimal(enterAmountField.getText());
+        } catch (NumberFormatException ex) {
+            JOptionPane.showMessageDialog(this, "Error: Please enter a valid number.");
+            return;
+        }
+
+        // Deposit
+        if (buttonPressed.equalsIgnoreCase("Deposit")) {
+            handleTransaction(buttonPressed, amountVal);
+        } else {
+            // Withdraw or transfer
+            int result = user.getCurrentBalance().compareTo(amountVal);
+            if (result < 0) {
+                JOptionPane.showMessageDialog(this, "Error: Input value is more than the current balance.");
+                return;
+            }
+
+            if (buttonPressed.equalsIgnoreCase("Withdraw")) {
+                handleTransaction(buttonPressed, amountVal);
+            } else {
+                String transferredUser = enterUserField.getText();
+                handleTransfer(user, transferredUser, amountVal);
+            }
+        }
+    }
 }
